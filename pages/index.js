b7c977e67556b65cddcc898b41ffc6ec66a01592
RPIUdishan <it18082234@my.sliit.lk>
2023-01-13 11:14:03 +0530

    SEARCH ISSUE

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,13 @@ export default function Home() {
       <Header />
       <MainImages />
       <div className="mt-8 flex items-center justify-center">
+        <a href="#search">
           <div className="border-solid border-white border-2 rounded-full animate-pulse cursor-pointer">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-12 h-12 text-white">
               <path fillRule="evenodd" d="M12 2.25a.75.75 0 01.75.75v16.19l6.22-6.22a.75.75 0 111.06 1.06l-7.5 7.5a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 111.06-1.06l6.22 6.22V3a.75.75 0 01.75-.75z" clipRule="evenodd" />
             </svg>
           </div>
+        </a>
       </div>
       <div id="search" className="mt-24">
         <Search />
